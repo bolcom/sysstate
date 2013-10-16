76522b99aa45af30693be710b7a734b66be076a3
Chris Kramer <ckramer@bol.com>
2013-10-16 15:29:58 +0200

    Updating version, constructing upgrade script

---
+++ mysql/upgrade_from_0.92.1.sql
@@ -0,0 +1,5 @@
+UPDATE SSE_PROPERTY SET PROP_KEY = "defaultView", PROP_GROUP = "nl.unionsoft.sysstate.configuration.ViewConfiguration", PROP_TYPE = "GROUP" WHERE PROP_KEY = "DEFAULT-VIEW";
+UPDATE SSE_PROPERTY SET PROP_KEY = "defaultTemplate", PROP_GROUP = "nl.unionsoft.sysstate.configuration.ViewConfiguration", PROP_TYPE = "GROUP" WHERE PROP_KEY = "DEFAULT-TEMPLATE";
+UPDATE SSE_PROPERTY SET PROP_KEY = "maintenanceMode", PROP_GROUP = "nl.unionsoft.sysstate.configuration.ViewConfiguration", PROP_TYPE = "GROUP" WHERE PROP_KEY = "MAINTENANCE-MODE";
+UPDATE SSE_PROPERTY SET PROP_KEY = "maintenanceMode", PROP_GROUP = "nl.unionsoft.sysstate.configuration.ViewConfiguration", PROP_TYPE = "GROUP" WHERE PROP_KEY = "MAINTENANCE-MODE";
+DELETE FROM SSE_PROPERTY WHERE PROP_KEY = "MAX_DAYS_TO_KEEP_STATES";
