c6b4a63ce62c0b70d63e68f5a1389d6c23353ad8
Chris Kramer <ckramer@bol.com>
2017-01-25 14:09:14 +0100

    Fixing tests
    Change-Id: If7ad69f8e72c5d80520138344ec9cd4f05531c7a

--- sysstate-groovy/src/test/groovy/nl/unionsoft/sysstate/plugins/groovy/consul/ConsulXpathInstanceResolverSpec.groovy
+++ sysstate-groovy/src/test/groovy/nl/unionsoft/sysstate/plugins/groovy/consul/ConsulXpathInstanceResolverSpec.groovy
@@ -9,6 +9,7 @@ import nl.unionsoft.sysstate.common.logic.EnvironmentLogic
 import nl.unionsoft.sysstate.common.logic.InstanceLinkLogic
 import nl.unionsoft.sysstate.common.logic.InstanceLogic
 import nl.unionsoft.sysstate.common.logic.ProjectLogic
+import nl.unionsoft.sysstate.common.logic.RelationalInstanceLogic;
 import nl.unionsoft.sysstate.common.logic.ResourceLogic
 import nl.unionsoft.sysstate.common.logic.TemplateLogic
 import nl.unionsoft.sysstate.plugins.http.HttpConstants
@@ -22,11 +23,12 @@ import spock.lang.Specification
 
 class ConsulXpathInstanceResolverSpec extends Specification{
 
-    
+
     ConsulXpathInstanceResolver consulPatternInstanceResolver;
-    
-    InstanceLogic instanceLogic = Mock()
-    InstanceLinkLogic instanceLinkLogic = Mock()
+
+
+    RelationalInstanceLogic relationalInstanceLogic = Mock();
+
     ResourceLogic resourceLogic = Mock()
     TemplateLogic templateLogic = Mock()
 
@@ -34,35 +36,35 @@ class ConsulXpathInstanceResolverSpec extends Specification{
     HttpResponse httpResponse = Mock(HttpResponse)
     StatusLine statusLine = Mock(StatusLine)
     HttpEntity httpEntity = Mock(HttpEntity)
-    
+
     void setup() {
-        consulPatternInstanceResolver = new ConsulXpathInstanceResolver( instanceLogic,  instanceLinkLogic,  resourceLogic,  templateLogic)
+        consulPatternInstanceResolver = new ConsulXpathInstanceResolver( relationalInstanceLogic,  resourceLogic,  templateLogic)
     }
-    
+
     def 'When I query consul, I expect instances to be created.'() {
         given:
         InstanceDto instance = new InstanceDto();
         instance.setConfiguration([
             "httpClientId"    : "test",
             "serverUrl"       : "http://localhost:8500",
-            "urlTemplate"     : "asdf",
+            "child_urlTemplate"     : "asdf",
             "servicesPattern" : "(?<environment>[a-z].*)-(?<application>wiz[a-z].*)-.*"
         ])
         StateDto state = new StateDto();
 
         def firstProject = new ProjectDto('name' : 'WIZFIRST', 'id':1)
         def secondProject = new ProjectDto('name': 'WIZSECOND', 'id':2)
-        
+
         def proEnvironment = new EnvironmentDto('name' : 'PRO', 'id' : 1)
         def testEnvironment = new EnvironmentDto('name' : 'TEST', 'id' : 2)
-        
+
         when:
         consulPatternInstanceResolver.setState(instance, state)
 
         then:
-        
-        1 *  instanceLinkLogic.getInstanceLinks(instance.getId()) >> []
-        
+
+        1 * relationalInstanceLogic.getChildInstances(instance) >> []
+
         then:
         1 * resourceLogic.getResourceInstance(HttpConstants.RESOURCE_MANAGER_NAME, "test") >> httpClient
         1 * httpClient.execute(_) >> httpResponse
@@ -70,18 +72,19 @@ class ConsulXpathInstanceResolverSpec extends Specification{
         1 * statusLine.getStatusCode() >> 200
         1 * httpResponse.getEntity() >> httpEntity
         1 * httpEntity.getContent() >> getClass().getResourceAsStream("/nl/unionsoft/sysstate/plugins/groovy/consul/services.json")
-        
+
         then:
         1 * templateLogic.writeTemplate("asdf", ['project':firstProject, 'environment' : proEnvironment], _)
         1 * templateLogic.writeTemplate("asdf", ['project':secondProject, 'environment' : testEnvironment], _)
-        
+
+        then:
+        2 * relationalInstanceLogic.findChildInstanceId([], _) >> Optional.empty()
+
         state.state == StateType.STABLE
         state.description == 'OK'
-        
-
     }
-    
+
     def createProject(name, id){
-        return 
+        return
     }
 }
