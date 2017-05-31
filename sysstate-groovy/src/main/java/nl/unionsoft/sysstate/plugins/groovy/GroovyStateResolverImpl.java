9c38ffbb9cd2644f9c969a9f5406b2ef90816f41
Chris Kramer <ckramer@bol.com>
2017-05-31 12:45:07 +0200

    Fixing build

--- sysstate-groovy/src/main/java/nl/unionsoft/sysstate/plugins/groovy/GroovyStateResolverImpl.java
+++ sysstate-groovy/src/main/java/nl/unionsoft/sysstate/plugins/groovy/GroovyStateResolverImpl.java
@@ -1,24 +1,23 @@
 package nl.unionsoft.sysstate.plugins.groovy;
 
-import groovy.lang.Binding;
-import groovy.lang.GroovyShell;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 import javax.inject.Inject;
 
-import nl.unionsoft.commons.util.PropertiesUtil;
-import nl.unionsoft.sysstate.common.dto.InstanceDto;
-import nl.unionsoft.sysstate.common.dto.StateDto;
-import nl.unionsoft.sysstate.common.extending.TimedStateResolver;
-
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import nl.unionsoft.commons.properties.PropertiesUtil;
+import nl.unionsoft.sysstate.common.dto.InstanceDto;
+import nl.unionsoft.sysstate.common.dto.StateDto;
+import nl.unionsoft.sysstate.common.extending.TimedStateResolver;
 
 
 @Service("groovyStateResolver")
